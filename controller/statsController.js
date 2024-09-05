526e4dbdf5743115218e772a55c62b0e2283966c
Doud75 <adrienquimbre@gmail.com>
2024-09-05 14:50:59 +0200

    add all stats

--- controller/statsController.js
+++ controller/statsController.js
@@ -1,51 +1,36 @@
 import {getSocketIOInstance} from "../socket.js";
 
-export async function getStats(req, res) {
+export async function getStatsByUser(req, res) {
   try {
     const playerId = req.params.playerId;
     const player = await req.server.pg.query(`
-          SELECT 
-            p."username",
-            (SELECT COUNT("playerRace"."id") FROM "playerRace" WHERE "playerRace"."playerId" = p."id") as nbRace,
-            (SELECT COUNT("race"."id") FROM "race" WHERE "race"."winner" = p."username") as nbVictory,
-            (SELECT FLOOR(AVG("race"."duration")) FROM "race" JOIN "playerRace" ON p."id" = "playerRace"."playerId" AND "playerRace"."raceId" = race."id") as avgDuration,
+      SELECT 
+        p."username",
+        (SELECT COUNT("playerRace"."id") FROM "playerRace" WHERE "playerRace"."playerId" = p."id") as nbRace,
+        (SELECT COUNT("race"."id") FROM "race" WHERE "race"."winner" = p."username") as nbVictory,
+        (SELECT FLOOR(AVG("race"."duration")) FROM "race" JOIN "playerRace" ON p."id" = "playerRace"."playerId" AND "playerRace"."raceId" = race."id") as avgDuration,
+        (
+          SELECT FLOOR (
             (
-              SELECT FLOOR (
-                (
-                  SELECT AVG("race"."duration") 
-                  FROM "race" 
-                    JOIN "playerRace" ON p."id" = "playerRace"."playerId"
-                    AND "playerRace"."raceId" = race."id"
-                ) / (
-                  SELECT SUM(CAST("race"."tourCount" as INT)) 
-                  FROM "race" 
-                    JOIN "playerRace" ON p."id" = "playerRace"."playerId"
-                    AND "playerRace"."raceId" = race."id"
-                )
-              )
-            ) as avgDurationPerTour
-          FROM "player" p
-          WHERE p."id" = $1;
-        `, [playerId]);
-    if (player.rows.length > 0) {
-        const statsGeneral = await req.server.pg.query(`
-          SELECT 
-            p."username",
-            (SELECT COUNT("playerRace"."id") FROM "playerRace" WHERE "playerRace"."playerId" = p."id") as nbRace,
-            (SELECT COUNT("race"."id") FROM "race" WHERE "race"."winner" = p."username") as nbVictory,
-            (SELECT FLOOR(AVG("race"."duration")) FROM "race" JOIN "playerRace" ON p."id" = "playerRace"."playerId" AND "playerRace"."raceId" = race."id") as avgDuration
-          FROM "player" p
-          ORDER BY nbVictory DESC
-          LIMIT 3
-          ;`);
-        res.send({
-          player : player.rows[0],
-          statsGeneral : statsGeneral.rows
-        });
-    }
-    else {
-      return res.status(404).send({ error: 'Player not found' });
-    }
+              SELECT AVG("race"."duration") 
+              FROM "race" 
+                JOIN "playerRace" ON p."id" = "playerRace"."playerId"
+                AND "playerRace"."raceId" = race."id"
+            ) / (
+              SELECT SUM(CAST("race"."tourCount" as INT)) 
+              FROM "race" 
+                JOIN "playerRace" ON p."id" = "playerRace"."playerId"
+                AND "playerRace"."raceId" = race."id"
+            )
+          )
+        ) as avgDurationPerTour
+      FROM "player" p
+      WHERE p."id" = $1;
+    `, [playerId]);
+    res.send({
+      player : player.rows[0],
+      statsGeneral : statsGeneral.rows
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: 'Internal Server Error' });
@@ -54,39 +39,19 @@ export async function getStats(req, res) {
 
 export async function getAllStats(req, res) {
     try {
-        // const result = await req.server.pg.query(`
-        //   SELECT "player"."username", count(r."winner") as nbRace
-        //   FROM "player"
-        //     LEFT JOIN "race" AS r ON "player"."username" = r."winner"
-        //     WHERE "player"."id" = 1
-        //   GROUP BY "player"."username";
-        // `);
-        const result = await req.server.pg.query(`
-          SELECT 
+      const statsGeneral = await req.server.pg.query(`
+        SELECT
             p."username",
             (SELECT COUNT("playerRace"."id") FROM "playerRace" WHERE "playerRace"."playerId" = p."id") as nbRace,
             (SELECT COUNT("race"."id") FROM "race" WHERE "race"."winner" = p."username") as nbVictory,
-            (SELECT FLOOR(AVG("race"."duration")) FROM "race" JOIN "playerRace" ON p."id" = "playerRace"."playerId" AND "playerRace"."raceId" = race."id") as avgDuration,
-            (
-              SELECT FLOOR (
-                (
-                  SELECT AVG("race"."duration") 
-                  FROM "race" 
-                    JOIN "playerRace" ON p."id" = "playerRace"."playerId"
-                    AND "playerRace"."raceId" = race."id"
-                ) / (
-                  SELECT SUM(CAST("race"."tourCount" as INT)) 
-                  FROM "race" 
-                    JOIN "playerRace" ON p."id" = "playerRace"."playerId"
-                    AND "playerRace"."raceId" = race."id"
-                )
-              )
-            ) as avgDurationPerTour
-          FROM "player" p
-          ORDER BY nbVictory DESC
-          LIMIT 3;
-        `);
-        res.send(result.rows);
+            (SELECT FLOOR(AVG("race"."duration")) FROM "race" JOIN "playerRace" ON p."id" = "playerRace"."playerId" AND "playerRace"."raceId" = race."id") as avgDuration
+        FROM "player" p
+        ORDER BY nbVictory DESC
+        LIMIT 3
+      ;`);
+      res.send({
+        statsGeneral : statsGeneral.rows
+      });
     } catch (err) {
         console.error(err);
         res.status(500).send({ error: 'Internal Server Error' });
--- routes/stats.js
+++ routes/stats.js
@@ -1,6 +1,6 @@
-import { getStats, getAllStats } from "../controller/statsController.js"
+import { getStatsByUser, getAllStats } from "../controller/statsController.js"
 
 export default async function (server) {
-  server.get('/get-stats/:playerId', getStats)
+  server.get('/get-stats/:playerId', getStatsByUser)
   server.get('/get-all-stats', getAllStats)
 }
